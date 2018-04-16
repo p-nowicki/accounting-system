69cd2e5eeea7d95a4d475fad87ac46e54cc3dfaa
lukasz <lukaszwojewodzki88@wp.pl>
2018-04-16 14:54:08 +0200

    change write and read methods , not correct code , when add new text it's overwriten in the same line

--- src/main/java/database/FileHelper.java
+++ src/main/java/database/FileHelper.java
@@ -1,7 +1,10 @@
 package database;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -10,7 +13,7 @@ public class FileHelper {
 
   public List<String> readLinesFromFile(String fileName) throws FileNotFoundException {
     List<String> line = new ArrayList<>();
-    try (Scanner scanner = new Scanner(new File(String.valueOf(fileName)))) {
+    try (Scanner scanner = new Scanner(new File(fileName))) {
       while (scanner.hasNextLine()) {
         line.add(scanner.nextLine());
       }
@@ -19,7 +22,26 @@ public class FileHelper {
     return line;
   }
 
-  public String procesLine(String line) {
-    return line;
+  public void writeFile(String id, String file) {
+    BufferedWriter writer = null;
+    try {
+
+      File logFile = new File("src/main/resources/file/newFile.txt");
+      System.out.println(logFile.getCanonicalPath());
+
+      writer = new BufferedWriter(new FileWriter(logFile, true));
+      writer.write(" Id number: " + id + " File name: " + file);
+
+    } catch (Exception exception) {
+      exception.printStackTrace();
+    } finally {
+      try {
+        if (writer != null) {
+          writer.close();
+        }
+      } catch (IOException exception) {
+        exception.printStackTrace();
+      }
+    }
   }
 }
--- src/main/java/database/Invoice.java
+++ src/main/java/database/Invoice.java
@@ -2,10 +2,35 @@ package database;
 
 public class Invoice {
 
+  private String name;
   private int id;
 
-  public Integer getId() {
+  public Invoice(String name, int id) {
+    this.name = name;
+    this.id = id;
+  }
 
+  public Invoice() {
+
+  }
+
+  private Invoice(String id) {
+
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getId() {
     return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
   }
 }
