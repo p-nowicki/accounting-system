49ff09494ba56db54afced80cad55e2a087cc71b
ja <1>
2018-04-18 21:56:07 +0200

    Merge branch 'setup' of https://github.com/CodersTrustPL/project-5-milosz-piotr-justyna-lukasz into setup
    # Conflicts:
    #	src/main/java/pl/coderstrust/accounting/Company.java
    #	src/test/java/pl/coderstrust/accounting/database/memory/InMemoryDatabaseTest.java
    #	src/test/java/pl/coderstrust/accounting/logic/InvoiceBookTest.java

---
+++ src/test/java/pl/coderstrust/accounting/database/file/InvoiceProvider.java
@@ -0,0 +1,77 @@
+package pl.coderstrust.accounting.database.file;
+
+import pl.coderstrust.accounting.model.Company;
+import pl.coderstrust.accounting.model.Invoice;
+
+public class InvoiceProvider {
+
+  public Invoice InvoiceOne () {
+
+    Company buyer = new Company();
+    buyer.setName("YoloCompany");
+    buyer.setNip(1231231212);
+    buyer.setStreet("Warszawska 1");
+    buyer.setPostCode("01-000");
+    buyer.setCity("Warszawa");
+
+    Company seller = new Company();
+    seller.setName("SellerCompanyOne");
+    seller.setNip(1233215678);
+    seller.setStreet("Poznanska 1");
+    seller.setPostCode("65-999");
+    seller.setCity("Poznań");
+
+    Invoice invoice = new Invoice();
+    invoice.setNumber("FV123/1");
+    //invoice.setIssueDate(); find how to add date
+    invoice.setBuyer(buyer);
+    invoice.setSeller(seller);
+    return invoice;
+  }
+  public Invoice InvoiceTwo () {
+
+    Company buyer = new Company();
+    buyer.setName("ABCCompany");
+    buyer.setNip(1212366991);
+    buyer.setStreet("Pawla 1");
+    buyer.setPostCode("01-000");
+    buyer.setCity("Warszawa");
+
+    Company seller = new Company();
+    seller.setName("AdamCompanyOne");
+    seller.setNip(2016545678);
+    seller.setStreet("Krakowska 1");
+    seller.setPostCode("65-999");
+    seller.setCity("Poznań");
+
+    Invoice invoice = new Invoice();
+    invoice.setNumber("FV123/2");
+    //invoice.setIssueDate(); find how to add date
+    invoice.setBuyer(buyer);
+    invoice.setSeller(seller);
+    return invoice;
+  }
+  public Invoice InvoiceThree () {
+
+    Company buyer = new Company();
+    buyer.setName("ABCCompany");
+    buyer.setNip(1212366991);
+    buyer.setStreet("Pawla 1");
+    buyer.setPostCode("01-000");
+    buyer.setCity("Warszawa");
+
+    Company seller = new Company();
+    seller.setName("KamilCompanyOne");
+    seller.setNip(1778886789);
+    seller.setStreet("Gdanska 1");
+    seller.setPostCode("65-999");
+    seller.setCity("Poznań");
+
+    Invoice invoice = new Invoice();
+    invoice.setNumber("FV123/3");
+    //invoice.setIssueDate(); find how to add date
+    invoice.setBuyer(buyer);
+    invoice.setSeller(seller);
+    return invoice;
+  }
+}
