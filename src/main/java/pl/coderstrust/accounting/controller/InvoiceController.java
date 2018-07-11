966094c44dec89c6d9268d52787e889d63266fce
Piotr Nowicki <piotradam.nowicki@gmail.com>
2018-07-11 20:37:31 +0200

    pdf generator finished

--- src/main/java/pl/coderstrust/accounting/controller/InvoiceController.java
+++ src/main/java/pl/coderstrust/accounting/controller/InvoiceController.java
@@ -1,8 +1,9 @@
 package pl.coderstrust.accounting.controller;
 
+import com.itextpdf.text.DocumentException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,24 +14,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.coderstrust.accounting.exceptions.InvoiceNotFoundException;
 import pl.coderstrust.accounting.logic.InvoiceService;
+import pl.coderstrust.accounting.logic.PdfGenerator;
 import pl.coderstrust.accounting.model.Invoice;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Optional;
 
 @RequestMapping("/invoices")
 @RestController
-@CrossOrigin
 public class InvoiceController {
 
   // TODO - add validator - class which is checking if provided invoice is the correct one
   // (all required fields are filled etc)
   private InvoiceService invoiceService;
 
+  private PdfGenerator pdfGenerator;
+
   @Autowired
-  public InvoiceController(InvoiceService invoiceService) {
+  public InvoiceController(InvoiceService invoiceService, PdfGenerator pdfGenerator) {
     this.invoiceService = invoiceService;
+    this.pdfGenerator = pdfGenerator;
   }
 
   @GetMapping
@@ -49,6 +54,23 @@ public class InvoiceController {
 
     return ResponseEntity.ok(invoiceOptional.get());
   }
+
+  @GetMapping("/{id}/pdf")
+  public ResponseEntity<?> getInvoicePdf(@PathVariable int id)
+      throws IOException, DocumentException {
+    Optional<Invoice> invoiceOptional = invoiceService.getInvoices().stream()
+        .filter(invoice -> invoice.getId().equals(id)).findAny();
+
+    if (!invoiceOptional.isPresent()) {
+      return ResponseEntity.notFound().build();
+    }
+
+    ByteArrayOutputStream pdf = pdfGenerator.generatePdfInvoice(invoiceOptional.get());
+    byte[] array = pdf.toByteArray();
+
+    return ResponseEntity.ok().contentType(MediaType.APPLICATION_PDF).body(array);
+  }
+
 
   @PostMapping
   public int saveInvoice(@RequestBody Invoice invoice) throws IOException {
@@ -72,7 +94,8 @@ public class InvoiceController {
   }
 
   @DeleteMapping("/{id}")
-  public ResponseEntity<?> removeInvoiceById(@PathVariable int id) throws InvoiceNotFoundException, IOException {
+  public ResponseEntity<?> removeInvoiceById(@PathVariable int id)
+      throws InvoiceNotFoundException, IOException {
 
     Optional<Invoice> invoiceOptional = invoiceService.getInvoices().stream()
         .filter(invoice -> invoice.getId().equals(id)).findAny();
